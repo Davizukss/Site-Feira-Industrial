2cb6d65a72868d9c602e955cf5a837d845a69bb5
Davi Varella <110793193+Davizukss@users.noreply.github.com>
2022-10-14 17:25:08 -0300

    refator

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const btnMobile = document.getElementById('btn-mobile');
 
 function toggleMenu(event) {
@@ -13,24 +13,5 @@ function toggleMenu(event) {
     event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
   }
 }
-
 btnMobile.addEventListener('click', toggleMenu);
-=======
-const btnMobile = document.getElementById('btn-mobile');
-
-function toggleMenu(event) {
-  if (event.type === 'touchstart') event.preventDefault();
-  const nav = document.getElementById('nav');
-  nav.classList.toggle('active');
-  const active = nav.classList.contains('active');
-  event.currentTarget.setAttribute('aria-expanded', active);
-  if (active) {
-    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
-  } else {
-    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
-  }
-}
-
-btnMobile.addEventListener('click', toggleMenu);
->>>>>>> bff5846a093e5a20261d458e9e71a92da030bec3
 btnMobile.addEventListener('touchstart', toggleMenu);
