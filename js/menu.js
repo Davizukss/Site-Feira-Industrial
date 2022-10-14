c55303e822cd1fc1ce4e01e4ba99180069d5ec73
Davi Varella <110793193+Davizukss@users.noreply.github.com>
2022-10-14 17:29:33 -0300

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,4 @@
-
 const btnMobile = document.getElementById('btn-mobile');
-
 function toggleMenu(event) {
   if (event.type === 'touchstart') event.preventDefault();
   const nav = document.getElementById('nav');
